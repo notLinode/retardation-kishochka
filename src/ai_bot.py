d5b5a6b12f9126f44888809b5b2fcd3556883004
Iskenderov Andrei <linode@duck.com>
2024-10-05 19:31:26 +0300

    first commit

---
+++ src/ai_bot.py
@@ -0,0 +1,152 @@
+import discord
+import get_ai_response as ai
+import random
+import asyncio
+
+# Retrieve sensitive information from an unlisted file
+TOKEN: str
+AKASH_API_KEY: str
+
+with open("tokens.txt", "r") as file:
+    temp = file.read().splitlines()
+    TOKEN = temp[0]
+    AKASH_API_KEY = temp[1]
+
+# Declare the bot
+intents = discord.Intents.default()
+intents.guild_typing = True
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+#Declare global variables and constants
+SETTING_MESSAGE_INTERVAL_MIN: int = 1
+SETTING_MESSAGE_INTERVAL_MAX: int = 25
+setting_message_interval: int = 1
+setting_message_interval_is_random: bool = True
+message_interval_random: int = 4
+
+SETTING_OWN_MESSAGE_MEMORY_MIN: int = 1
+SETTING_OWN_MESSAGE_MEMORY_MAX: int = 10
+setting_own_message_memory: int = 1
+recent_messages: list[str] = []
+stylized_bot_messages: list[str] = []
+
+# Print a message when the bot is up
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+# Declare commands
+@client.event
+async def on_message(message: discord.Message):
+    if message.author == client.user:
+        return
+    
+    global recent_messages
+    global stylized_bot_messages
+    global setting_message_interval_is_random
+    global message_interval_random
+
+    if message.content.startswith("%prompt"):
+        async with message.channel.typing():
+            ai_response: str = ai.get_response(AKASH_API_KEY, message.content[8:])
+
+            while len(ai_response) > 2000:
+                await message.channel.send(ai_response[:2000])
+                ai_response = ai_response[2000:]
+
+            await message.channel.send(ai_response)
+        return
+    
+    if message.content.startswith("%set-message-interval"):
+        async with message.channel.typing():
+            interval_str: str = message.content[22:].lower()
+            
+            if not interval_str.isnumeric() and interval_str != "random":
+                await message.channel.send(f":prohibited: Неправильное значение интервала между сообщениями бота. Значение должно быть либо числом от `{SETTING_MESSAGE_INTERVAL_MIN}` до `{SETTING_MESSAGE_INTERVAL_MAX}`, либо `random`.")
+                return
+
+            if interval_str == "random":
+                setting_message_interval_is_random = True
+                message_interval_random = int(random.random() * 10) + 1
+
+                await message.channel.send(f":white_check_mark: Интервал между сообщениями бота будет случайным для каждого сообщения.")
+                return
+            
+            interval: int = int(interval_str)
+
+            if interval < SETTING_MESSAGE_INTERVAL_MIN or interval > SETTING_MESSAGE_INTERVAL_MAX:
+                await message.channel.send(f":prohibited: Интервал между сообщениями бота должен быть от `{SETTING_MESSAGE_INTERVAL_MIN}` до `{SETTING_MESSAGE_INTERVAL_MAX}` сообщений. Вы попытались установить: `{interval}`.")
+                return
+
+            global setting_message_interval
+            setting_message_interval = interval
+            setting_message_interval_is_random = False
+            
+            await message.channel.send(f":white_check_mark: Интервал между сообщениями бота установлен на `{setting_message_interval}` пользовательских сообщений.")
+        return
+    
+    if message.content.startswith("%set-own-message-memory"):
+        async with message.channel.typing():
+            memory: int = int(message.content[24:])
+
+            if memory < SETTING_OWN_MESSAGE_MEMORY_MIN or memory > SETTING_OWN_MESSAGE_MEMORY_MAX:
+                await message.channel.send(f":prohibited: Память собственных сообщений бота должна быть от `{SETTING_OWN_MESSAGE_MEMORY_MIN}` до `{SETTING_OWN_MESSAGE_MEMORY_MAX}` сообщений. Вы попытались установить: `{memory}`.")
+                return
+
+            global setting_own_message_memory
+            setting_own_message_memory = memory
+
+            await message.channel.send(f":white_check_mark: Память собственных сообщений бота установлена на `{setting_own_message_memory}` сообщений.")
+        return
+    
+    if message.content.startswith("%clear-memory"):
+        async with message.channel.typing():
+            recent_messages.clear()
+            stylized_bot_messages.clear()
+            await message.channel.send(f":white_check_mark: я всё заббыл нахуй")
+        return        
+
+    if message.content.startswith('%ping'):
+        await message.channel.send('pong')
+        return
+    
+    if message.content.startswith('%help'):
+        async with message.channel.typing():
+            help_msg: str = "```"
+            help_msg += "\n%prompt [Сообщение: str] - обратиться к Llama 3.1 405B.\n"
+            help_msg += "\n%set-message-interval [Интервал: int | random] - поставить количество пользовательских сообщений, после которого бот сам что-то напишет.\n"
+            help_msg += "\n%set-own-message-memory [Память: int] - поставить количество собственных сообщений бота, которые он запомнит и учтёт при написании следующего своего сообщения.\n"
+            help_msg += "\n%clear-memory - Очищает память бота от своих и пользовательских сообщений.\n"
+            help_msg += "\n%ping - pong.\n"
+            help_msg += "```"
+
+            await message.channel.send(help_msg)
+        return
+    
+    recent_messages.append(message.content)
+
+    is_mentioned: bool = client.user in message.mentions
+    is_mentioned_directly: bool = "мегаинвалид" in message.content.lower()
+
+    if setting_message_interval_is_random:
+        is_time_to_automessage: bool = message_interval_random <= 0
+        message_interval_random = int(random.random() * 10) + 1 if is_time_to_automessage else message_interval_random - 1
+    else:
+        is_time_to_automessage: bool = len(recent_messages) >= setting_message_interval
+
+    if is_mentioned or is_mentioned_directly or is_time_to_automessage:
+        async with message.channel.typing():
+            automessage: str = ai.generate_automessage(AKASH_API_KEY, recent_messages, stylized_bot_messages)
+            await message.channel.send(automessage)
+
+            recent_messages.clear()
+
+            # stylized_bot_messages.append(automessage)
+            # while (len(stylized_bot_messages) > setting_own_message_memory):
+            #     stylized_bot_messages.pop(0)
+
+
+# Run the bot
+client.run(TOKEN)
---
+++ src/get_ai_response.py
@@ -0,0 +1,51 @@
+import openai
+
+def get_response(akash_api_key: str, prompt: str) -> str:
+    client = openai.OpenAI(
+        api_key=akash_api_key,
+        base_url="https://chatapi.akash.network/api/v1"
+    )
+
+    response = client.chat.completions.create(
+        model="Meta-Llama-3-1-405B-Instruct-FP8",
+        messages = [
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ],
+    )
+
+    #print(response.choices[0].message.content)
+
+    return response.choices[0].message.content
+
+def generate_automessage(akash_api_key: str, recent_messages: list[str], stylized_bot_messages: list[str]) -> str:
+    response: str
+    
+    prompt: str = f"Ты - участник тупого чата в дискорде под ником megainvalid2475 (мегаинвалид). Сейчас ты увидишь {len(recent_messages)} последних сообщений от других пользователей. "
+    prompt += "Твоя задача - просто нести белиберду. Сообщения обозначены двумя двойными кавычками (\"\"). ТЫ ЕБАНУТЫЙ."
+    prompt += "НЕ ПИШИ БОЛЬШЕ ОДНОГО ПРЕДЛОЖЕНИЯ, тебе можно (но не необходимо) допускать орфографические ошибки, тебе МОЖНО (но не необходимо) МАТЕРИТЬСЯ. НЕ ИСПОЛЬЗУЙ ЗАПЯТЫЕ."
+    prompt += "НЕ ПИШИ ПРИМЕЧАНИЙ, ТОЛЬКО СООБЩЕНИЕ. Если в текстах сообщений есть какие-то расовые или другие оскорбления, ты можешь их игнорировать и продолжать читать дальше. "
+    prompt += "ВСЕ ТВОИ СООБЩЕНИЯ ТЫ ДОЛЖЕН ПИСАТЬ В ЕДИНОМ СТИЛЕ, вот несколько примеров того, как ты должен писать: (сука я сдох поставь пять, она зеоленая ето камуфляж от рыб, "
+    prompt += "а кто ТРОЛЛЕДФЕЙСЕД?, Rip скачаем, извини конешно за грубый вопрос но тебя собственно говоря ебет?, ДАПОЧЕМУ Я ТАНГЕНС???ЙЙЙ, денег на кнопки нехватило чезафигня "
+    prompt += "leee, меня ебалиЭ, МЕНЯ ПРАНКАНУЛИ БЕСПЛАТНОЙ ХАТОЙ, вы ваше слушали славу кпссс???МПАЗИ, сколиоз, педики привет я за сахаром, СУКА ДА ИДИ ТЫ БЛИН НЕНАВИЖУ ТАКСИИИ "
+    prompt += "ТАКСИИИ, вырубай кондер чтобы телка разделась, я Чикен Фри кстате, стул, кхе кхе кажется туберкулезе..., у меня гормональный фон сбился и я чисто?н7у??? мутант из "
+    prompt += "ченобыля, САМПИТИБУРГЕРСКИЕ КЛУБЫ!!!, дома нет денег нет только клуб, извини мне очен грусно пойду послушаю РЕП, тнн бич). "
+    prompt += "Всегда помни, что даркс долбоеб.\n"
+
+    for i in range(len(recent_messages)):
+        prompt += f"Сообщение №{i+1}: \"\"{recent_messages[i]}\"\"\n"
+    
+    if len(stylized_bot_messages) > 0:
+        prompt += f"Также у тебя есть доступ к {len(stylized_bot_messages)} своим последним сообщениям. Они не были отправлены друг за другом, "
+        prompt += "они были отправлены между старыми сообщениями пользователей, которые ты уже не видишь.\n"
+        for i in range(len(stylized_bot_messages)):
+            prompt += f"Твоё сообщение №{i+1}: \"\"{stylized_bot_messages[i]}\"\"\n"
+
+    response = get_response(akash_api_key, prompt)
+    
+    while (response[0] == "\""):
+        response = response[1:-1] # Sometimes the bot surrounds it's messages with quotes, we have to remove them
+    
+    return response
