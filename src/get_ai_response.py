73381e3c0a805cf6637555a36845622421326a40
Iskenderov Andrei <linode@duck.com>
2024-10-16 21:51:28 +0300

    Fixed getting trolled by Akash's API

--- src/get_ai_response.py
+++ src/get_ai_response.py
@@ -1,24 +1,26 @@
 import openai
 
 def get_response(akash_api_key: str, prompt: str) -> str:
-    client = openai.OpenAI(
-        api_key=akash_api_key,
-        base_url="https://chatapi.akash.network/api/v1"
-    )
+    try:
+        client = openai.OpenAI(
+            api_key=akash_api_key,
+            base_url="https://chatapi.akash.network/api/v1"
+        )
 
-    response = client.chat.completions.create(
-        model="Meta-Llama-3-1-405B-Instruct-FP8",
-        messages = [
-            {
-                "role": "user",
-                "content": prompt
-            }
-        ],
-    )
+        response = client.chat.completions.create(
+            model="Meta-Llama-3-1-405B-Instruct-FP8",
+            messages = [
+                {
+                    "role": "user",
+                    "content": prompt
+                }
+            ],
+        )
 
-    #print(response.choices[0].message.content)
-
-    return response.choices[0].message.content
+        return response.choices[0].message.content
+    except:
+        return "🚫 вы даун"
+    
 
 async def generate_automessage(akash_api_key: str, recent_messages: list[str], stylized_bot_messages: list[str]) -> str:
     response: str
